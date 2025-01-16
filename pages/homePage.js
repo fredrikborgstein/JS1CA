1a258235aa470a5b8906d41f138c71dbfb6024ae
Fredrik Borgstein <fredrik.borgstein@outlook.com>
2025-01-16 22:39:26 +0100

    Functionality added to home page, needs better styling, and product links dont work completely yet

--- pages/homePage.js
+++ pages/homePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
                   <h1>50 % DISCOUNT FOR NEW MEMBERS!</h1>
                 </div>
                 <div id="hero-section-button">
-                  <a href="./register.html" class="btn hero-section-button">Register</a>
+                  <a href="/register" class="btn hero-section-button">Register</a>
                 </div>
               </section>
               <!-- PROMOTIONS SECTION -->
@@ -33,7 +33,7 @@ const HomePage = () => {
                 </div>
               </section>
               <div id="promotion-section-button">
-                <a href="./allproducts.html" class="btn promotion-section-btn">
+                <a href="/allProducts" class="btn promotion-section-btn">
                   Browse All Games
                 </a>
               </div>
@@ -93,14 +93,142 @@ const HomePage = () => {
                   </div>
                 </div>
                 <div id="members-section-button">
-                  <a href="./register.html" class="members-section-button-text btn">
+                  <a href="/register" class="members-section-button-text btn">
                     Become a member
                   </a>
                 </div>
               </section>
             </div>`,
         onMount: () => {
-            // Actions to perform when the component is mounted
+            const apiUrl = "https://v2.api.noroff.dev/";
+            const newReleasesContainer = document.getElementById(
+              "new-releases-cards-container"
+            );
+            const popularProductsContainer = document.getElementById(
+              "bestsellers-cards-container"
+            );
+
+            const productBtns = document.getElementsByClassName("card-text");
+            for (const btn of productBtns) {
+                btn.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    const productId = e.target.dataset.id;
+
+                    if (productId) {
+                        window.location.href = `/productpage?id=${productId}`;
+                    } else {
+                    }
+                });
+            }
+
+            async function getAllProducts() {
+                const cacheKey = "productsCache";
+                const cacheExpiration = 60 * 60 * 1000;
+
+                const cachedData = localStorage.getItem(cacheKey);
+                const cachedTimestamp = localStorage.getItem(`${cacheKey}-timestamp`);
+
+                if (cachedData && cachedTimestamp) {
+                    const now = Date.now();
+                    if (now - parseInt(cachedTimestamp, 10) < cacheExpiration) {
+                        const cachedObject = JSON.parse(cachedData);
+                        const products = cachedObject.data;
+                        if (!Array.isArray(products)) {
+                            return;
+                        }
+                        const bestSellers = products.slice(0, 3);
+                        const newReleases = products.slice(3, 6);
+
+                        processProducts({ bestSellers, newReleases });
+                        return;
+                    }
+                }
+
+                const response = await fetch(`${apiUrl}gamehub`);
+                const data = await response.json();
+
+                const products = data.data;
+                if (!Array.isArray(products)) {
+                    return;
+                }
+
+                const bestSellers = products.slice(0, 3);
+                const newReleases = products.slice(3, 6);
+
+                localStorage.setItem(cacheKey, JSON.stringify(data));
+                localStorage.setItem(`${cacheKey}-timestamp`, Date.now().toString());
+                processProducts({ bestSellers, newReleases });
+            }
+
+
+            function processProducts(products) {
+                const newReleases = products.newReleases;
+                const bestSellers = products.bestSellers;
+                createHomePageProductCards(newReleases, "releases");
+                createHomePageProductCards(bestSellers, "bestsellers");
+            }
+
+            function createHomePageProductCards (products, element)  {
+                products.forEach((product) => {
+                    const newReleaseCard = document.createElement("div");
+                    newReleaseCard.classList.add("card");
+                    newReleaseCard.id = product.id;
+                    const tags = product.tags;
+
+                    const newReleaseCardTitleContainer = document.createElement("div");
+                    const newReleaseCardTitle = document.createElement("h3");
+                    newReleaseCardTitle.innerText = product.title;
+                    newReleaseCardTitleContainer.appendChild(newReleaseCardTitle);
+                    newReleaseCard.appendChild(newReleaseCardTitleContainer);
+
+                    const newReleaseImageContainer = document.createElement("div");
+                    newReleaseImageContainer.classList.add("card-image");
+                    const newReleaseCardImage = document.createElement("img");
+                    newReleaseCardImage.src = product.image.url;
+                    newReleaseCardImage.alt = product.image.alt;
+                    newReleaseImageContainer.appendChild(newReleaseCardImage);
+                    newReleaseCard.appendChild(newReleaseImageContainer);
+
+                    const newReleaseCardDescriptionContainer = document.createElement("div");
+                    newReleaseCardDescriptionContainer.classList.add("card-info");
+
+                    const newReleaseCardTags = document.createElement("p");
+                    tags.forEach((tag) => {
+                        const newReleaseCardTag = document.createElement("span");
+                        newReleaseCardTag.classList.add("card-tag");
+                        newReleaseCardTag.style.fontSize = "0.8rem";
+                        newReleaseCardTag.innerText = tag + " ";
+                        newReleaseCardTags.appendChild(newReleaseCardTag);
+                    });
+                    newReleaseCardDescriptionContainer.appendChild(newReleaseCardTags);
+
+                    const newReleaseCardDescriptionPrice = document.createElement("p");
+                    newReleaseCardDescriptionPrice.innerText = product.price;
+                    newReleaseCardDescriptionContainer.appendChild(
+                      newReleaseCardDescriptionPrice
+                    );
+                    newReleaseCard.appendChild(newReleaseCardDescriptionContainer);
+
+                    const newReleaseCardButtonContainer = document.createElement("div");
+                    newReleaseCardButtonContainer.classList.add("card-button");
+                    const newReleaseCardButton = document.createElement("a");
+                    newReleaseCardButton.classList.add("card-text");
+                    newReleaseCardButton.innerText = "View Product";
+                    newReleaseCardButton.href = `#`;
+                    newReleaseCardButton.setAttribute("data-id", product.id);
+                    newReleaseCardButtonContainer.appendChild(newReleaseCardButton);
+                    newReleaseCard.appendChild(newReleaseCardButtonContainer);
+
+                    if (element === 'releases') {
+                        newReleasesContainer.appendChild(newReleaseCard);
+                    } else if (element === 'bestsellers') {
+                        popularProductsContainer.appendChild(newReleaseCard);
+                    }
+
+                });
+            }
+
+            void getAllProducts();
         },
         onDismount: () => {
             // Actions to perform when the component is dismounted
